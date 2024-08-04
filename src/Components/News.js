e84e18a7dbe4930ab0d2fa79c054955d6fae6aaa
DarshanCHMSR <chdarshan99@gmail.com>
2024-08-04 11:37:25 +0530

    conversation of class component to function based component

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,108 +1,77 @@
-import React, { Component } from "react";
+import React, {useEffect,useState}from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
+import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
 
 // import PropTypes from 'prop-types'
-
-export class News extends Component {
-  static defaultProps = {
-    country: "in",
-    pageSize: 8,
-    category: "general",
-  };
-
-  //   static PropTypes = {
-  //   country:PropTypes.string,
-  //   pageSize:PropTypes.number,
-  //   category:PropTypes.string,
-  // }
-
-  articles = [
-    // {
-    //     "source": {
-    //         "id": "bbc-sport",
-    //         "name": "BBC Sport"
-    //     },
-    //     "author": null,
-    //     "title": "England vs New Zealand LIVE: Second women’s T20 – cricket score, radio commentary, video highlights and text updates",
-    //     "description": "England host New Zealand in the second women's T20 at Hove – follow text updates, radio commentary and video highlights.",
-    //     "url": "http://www.bbc.co.uk/sport/cricket/live/cqee94r3687t",
-    //     "urlToImage": "https://static.files.bbci.co.uk/ws/simorgh-assets/public/sport/images/metadata/poster-1024x576.png",
-    //     "publishedAt": "2024-07-09T17:07:18.2249256Z",
-    //     "content": "<li>England face New Zealand in second T20 at Hove\r\n</li><li>England lead five-match series 1-0 after 59-run win on Saturday\r\n</li><li>Both sides building up to October's T20 World Cup in Bangladesh\r… [+191 chars]"
-    // },
-    // {
-    //     "source": {
-    //         "id": "espn-cric-info",
-    //         "name": "ESPN Cric Info"
-    //     },
-    //     "author": null,
-    //     "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-    //     "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-    //     "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-    //     "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-    //     "publishedAt": "2020-04-27T11:41:47Z",
-    //     "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    // },
-    // {
-    //     "source": {
-    //         "id": "espn-cric-info",
-    //         "name": "ESPN Cric Info"
-    //     },
-    //     "author": null,
-    //     "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-    //     "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-    //     "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-    //     "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-    //     "publishedAt": "2020-03-30T15:26:05Z",
-    //     "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    //  }
-  ];
-  capitilizeFirstLetter = (string) => {
+const News =(props)=> {
+ const [articles, setarticles] = useState([])
+ const [loading, setLoading] = useState(true)
+ const [page, setPage] = useState(1)
+ const [totalResults, setTotalResults] = useState(0)
+ const capitilizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
-  constructor(props) {
-    super(props);
-    this.state = {
-      articles: this.articles,
-      loading: false,
-      page: 1,
-      totalResults: 0,
-    };
-    document.title =
-      "DAILYNEWS-" + this.capitilizeFirstLetter(this.props.category);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {              ---- class based
+  //     articles: this.articles,
+  //     loading: false,
+  //     page: 1,
+  //     totalResults: 0,
+  //   };
+    // document.title =
+    //   "DAILYNEWS-" + capitilizeFirstLetter(props.category);
+  
 
-  async updateNews(pageNo) {
-    this.props.setProgress(0);
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.state.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+ const updateNews= async() =>{
+    props.setProgress(0);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=419440d3e346449188b1f8b2d8977afe&page=${page}&pageSize=${props.pageSize}`;
+    // this.setState({ loading: true });
+    setLoading(true);
     let data = await fetch(url);
-    this.props.setProgress(50);
+    props.setProgress(50);
     let parsedData = await data.json();
-    this.props.setProgress(70);
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-    });
-    this.props.setProgress(100);
+    props.setProgress(70);
+    setarticles(parsedData.articles)
+    setTotalResults(parsedData.totalResults)
+    setLoading(false)
+    // this.setState({
+    //   articles: parsedData.articles,
+    //   totalResults: parsedData.totalResults,
+    //   loading: false,
+    // });
+    props.setProgress(100);
   }
+  // useEffect(() => {
+  //  updateNews();
+  // },)
+  useEffect(() => {
+    // document.title = `${this.capitalizeFirstLetter(props.category)} - NewsMonkey`;
+    updateNews(); 
+    // eslint-disable-next-line
+}, [])
 
-  async componentDidMount() {
-    //  let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=419440d3e346449188b1f8b2d8977afe&page-1&pageSize=${this.props.pageSize}`;
-    //  this.setState({loading:true});
-    //  let data = await fetch(url);
-    //  let parsedData = await data.json()
-    //  this.setState({articles:parsedData.articles,
-    //   totalResults:parsedData.totalResults,
-    //   loading:false
-    // })
-    this.updateNews();
-  }
+  // async componentDidMount() {
+  //    let url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=419440d3e346449188b1f8b2d8977afe&page-1&pageSize=${props.pageSize}`;
+  //    this.setState({loading:true});
+  //    let data = await fetch(url);
+  //    let parsedData = await data.json()
+  //    this.setState({articles:parsedData.articles,
+  //     totalResults:parsedData.totalResults,
+  //     loading:false
+  //   })
+  //   this.updateNews();
+  // }
+
+  // const handlePrevsClick = async () => {
+  //   // this.setState({page:this.state.page-1});
+  //   setPage(page-1)
+  //   updateNews();
+  // }
   // handlePrevsClick = async () => {
-  //     let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=419440d3e346449188b1f8b2d8977afe&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+  //     let url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=419440d3e346449188b1f8b2d8977afe&page=${this.state.page-1}&pageSize=${props.pageSize}`;
   //     this.setState({loading:true});
   //     let data = await fetch(url);
   //     let parsedData = await data.json()
@@ -117,10 +86,16 @@ export class News extends Component {
   //   });
   //   this.updateNews();
   // };
+  // const handleNextClick = async () => {
+  //   // this.setState({page:this.state.page+1});
+  //   setPage(page+1)
+  //   updateNews();
+  // }
+
   // handleNextClick = async () => {
-  //       if(!(this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize))){
+  //       if(!(this.state.page+1>Math.ceil(this.state.totalResults/props.pageSize))){
   //         this.setState({loading:true});
-  //        { let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=419440d3e346449188b1f8b2d8977afe&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+  //        { let url=`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=419440d3e346449188b1f8b2d8977afe&page=${this.state.page+1}&pageSize=${props.pageSize}`;
   //         let data = await fetch(url);
   //         let parsedData = await data.json()
   //         this.setState({
@@ -135,37 +110,40 @@ export class News extends Component {
   //   this.updateNews();
   // };
 
-  fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.state.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+  const fetchMoreData = async () => {
+    // this.setState({ page: this.state.page + 1 });
     
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=419440d3e346449188b1f8b2d8977afe&page=${page+1}&pageSize=${props.pageSize}`;
+    setPage(page+1)
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults
-    });
+    setarticles(articles.concat(parsedData.articles))
+    setTotalResults( parsedData.totalResults)
+  //   this.setState({
+  //     articles: articles.concat(parsedData.articles),
+  //     totalResults: parsedData.totalResults
+  //   });
   };
 
-  render() {
+ 
     return (
       <>
       <div className="container my-3">
-        <h1 className="text-center" style={{ margin: "35px" }}>
-          DailyNews-Top {this.capitilizeFirstLetter(this.props.category)}{" "}
+        <h1 className="text-center" style={{ margin: "35px" ,marginTop:"90px"}}>
+          DailyNews-Top {capitilizeFirstLetter(props.category)}{" "}
           HeadLines
         </h1>
-        {this.state.loading && <Spinner />}
+        {loading && <Spinner />}
         <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
+          dataLength={articles.length}
+          next={fetchMoreData}
+          hasMore={articles.length !== totalResults}
           loader={<Spinner />}
         >
           <div className="container">
           <div className="row">
-            {!this.state.loading &&
-              this.state.articles.map((element) => {
+            {!loading &&
+              articles.map((element) => {
                 return (
                   <div className="col-md-4" key={element.url}>
                     <NewsItem
@@ -195,7 +173,7 @@ export class News extends Component {
             type="button"
             disabled={
               this.state.page + 1 >
-              Math.ceil(this.state.totalResults / this.props.pageSize)
+              Math.ceil(this.state.totalResults / props.pageSize)
             }
             className="btn btn-dark"
             onClick={this.handleNextClick}
@@ -209,6 +187,16 @@ export class News extends Component {
       </>
     );
   }
-}
+  
+    News.defaultProps = {
+      country: "in",
+      pageSize: 8,
+      category: "general",
+    };
+    News.propTypes = {
+      country: PropTypes.string,
+      pageSize: PropTypes.number,
+      category: PropTypes.string,
+  }
 
 export default News;
